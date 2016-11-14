fd68a988b62acdbeeb6344ddcdd0d61f82bdc147
Adrien <adrien@gsell.me>
2016-11-14 15:09:16 +0100

    Put logStatus.isLogged instead of just isLogged

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,47 +1,24 @@
 angular.module('starter.controllers', [])
 
 .controller('TabCtrl', function($scope, $stateParams, $ionicModal, logStatus) {
-    $scope.logStatus = logStatus;
-    $scope.isLogged = logStatus.isLogged;
-
-    $scope.$watch('logStatus.isLogged', function (newVal, oldVal, scope) {
-      if(newVal) { 
-        scope.isLogged = newVal;
-      }
-    });
+  $scope.logStatus = logStatus;
 })
 
 
 .controller('HomeCtrl', function($scope, $stateParams, $ionicModal, logStatus) {
-    $scope.logStatus = logStatus;
-    $scope.isLogged = logStatus.isLogged;
-
-    $scope.$watch('logStatus.isLogged', function (newVal, oldVal, scope) {
-      if(newVal) { 
-        scope.isLogged = newVal;
-      }
-    });
+  $scope.logStatus = logStatus;
 })
 
 .controller('StateCtrl', function($scope, $stateParams, $ionicModal, logStatus) {
-
   $scope.logStatus = logStatus;
-  $scope.isLogged = logStatus.isLogged;
-
-  $scope.$watch('logStatus.isLogged', function (newVal, oldVal, scope) {
-    if(newVal) { 
-      scope.isLogged = newVal;
-    }
-  });
-
 })
 
 .controller('LoginCtrl', function($scope, $stateParams, $ionicModal, Users, Cards, logStatus, currentId) {
 
   // finduser
   $ionicModal.fromTemplateUrl('templates/modallogin.html', {
-  scope: $scope,
-  animation: 'slide-in-up'
+    scope: $scope,
+    animation: 'slide-in-up'
   }).then(function(modal) {
     $scope.modallogin = modal;
   });
@@ -55,7 +32,7 @@ angular.module('starter.controllers', [])
 
   $scope.finduser = function(login, password) {
     return Users.finduser(login, password) //checkuser returns the userId
-    .then(function(response) {
+      .then(function(response) {
 
       //tests
       console.log(response);
@@ -80,8 +57,8 @@ angular.module('starter.controllers', [])
 
       $scope.closeModallogin();
     })
-    .then(undefined, function(error) {
-        console.log('ERR services > Users : ', error.message);
+      .then(undefined, function(error) {
+      console.log('ERR services > Users : ', error.message);
     });
   }
 
@@ -105,7 +82,7 @@ angular.module('starter.controllers', [])
 
   $scope.createuser = function(login, password) {
     return Users.createuser(login, password)
-    .then(function(response) {
+      .then(function(response) {
 
       //tests
       console.log("User", user);
@@ -142,7 +119,7 @@ angular.module('starter.controllers', [])
       scope.userid = newVal;
       userid = newVal;
       Cards.all(newVal)
-      .then(function(response){
+        .then(function(response){
         scope.cards = response;
       })
     }
@@ -159,16 +136,16 @@ angular.module('starter.controllers', [])
 
   $scope.openModallinkus = function() {
 
-  $scope.$watch('currentId.userId', function (newVal, oldVal, scope) {
-    if(newVal) {
-      scope.userid = newVal;
-      userid = newVal;
-      Cards.all(newVal)
-      .then(function(response){
-        scope.cards = response;
-      })
-    }
-  });
+    $scope.$watch('currentId.userId', function (newVal, oldVal, scope) {
+      if(newVal) {
+        scope.userid = newVal;
+        userid = newVal;
+        Cards.all(newVal)
+          .then(function(response){
+          scope.cards = response;
+        })
+      }
+    });
 
     $scope.modallinkus.show();
   };
@@ -177,21 +154,21 @@ angular.module('starter.controllers', [])
   };
 
   $scope.linkus = function(myCardId) {
-    
+
     var posOptions = {timeout: 10000, enableHighAccuracy: false};
-    
-   $cordovaGeolocation
-   .getCurrentPosition(posOptions)	
-   .then(function (position) {
+
+    $cordovaGeolocation
+      .getCurrentPosition(posOptions)
+      .then(function (position) {
       var lat  = position.coords.latitude;
       var long = position.coords.longitude;
       console.log(lat + '   ' + long);
       $scope.lati = lat;
       $scope.lng = long;
-   }, function(err) {
+    }, function(err) {
       console.log(err);
-   });
-      
+    });
+
     return Links.createlink(myCardId, userid, $scope.lati ,$scope.lng).
     then(function(response){
       console.log(response);
@@ -207,46 +184,39 @@ angular.module('starter.controllers', [])
 })
 
 .controller('ContactsCtrl', function($scope, $stateParams, $ionicModal, Users, Cards, Contacts, logStatus, currentId) {
-    $scope.contacts = [];
-    // userId = currentId.userId; //localStorage.getItem('userid', response);
-    // userId = parseInt(localStrage.getItem('userid'));
+  $scope.contacts = [];
+  // userId = currentId.userId; //localStorage.getItem('userid', response);
+  // userId = parseInt(localStrage.getItem('userid'));
 
-    /*
+  /*
     Contacts.all(userId) //checkuser returns the userId
     .then(function(response) {
         $scope.contacts = response;
     })
     */
 
-    $scope.logStatus = logStatus;
-    $scope.isLogged = logStatus.isLogged;
+  $scope.logStatus = logStatus;
 
-    $scope.$watch('logStatus.isLogged', function (newVal, oldVal, scope) {
-      if(newVal) { 
-        scope.isLogged = newVal;
-      }
-    });
+  $scope.currentId = currentId;
+  $scope.userid = currentId.userId;
 
-    $scope.currentId = currentId;
-    $scope.userid = currentId.userId;
+  $scope.$watch('currentId.userId', function (newVal, oldVal, scope) {
+    if(newVal) {
+      console.log("hey" + newVal);
 
-    $scope.$watch('currentId.userId', function (newVal, oldVal, scope) {
-      if(newVal) {
-         console.log("hey" + newVal);
-        
-        $scope.userid = newVal;
-        Contacts.all(newVal) //checkuser returns the userId
+      $scope.userid = newVal;
+      Contacts.all(newVal) //checkuser returns the userId
         .then(function(response) {
-          scope.contacts = response;
-        })
-      }
-    });
+        scope.contacts = response;
+      })
+    }
+  });
 
-}) 
+})
 
 
 .controller('ContactDetailCtrl', function($scope, $stateParams, Contacts, logStatus) {
-  $scope.contact = Contacts.get($stateParams.contactCardId);  
+  $scope.contact = Contacts.get($stateParams.contactCardId);
   //$scop.loc = Contacts.getloca($scope.contact);
 })
 
@@ -258,30 +228,23 @@ angular.module('starter.controllers', [])
   $scope.userid = currentId.userId;
   var userId = currentId.userId;
 
-    $scope.$watch('currentId.userId', function (newVal, oldVal, scope) {
-      if(newVal) {
-        // console.log("hey" + newVal);
-        scope.userid = newVal;
-        userId = newVal;
-        Cards.all(newVal) //checkuser returns the userId
+  $scope.$watch('currentId.userId', function (newVal, oldVal, scope) {
+    if(newVal) {
+      // console.log("hey" + newVal);
+      scope.userid = newVal;
+      userId = newVal;
+      Cards.all(newVal) //checkuser returns the userId
         .then(function(response) {
-          scope.cards = response;
-        })
-      }
-    });
+        scope.cards = response;
+      })
+    }
+  });
 
-    $scope.logStatus = logStatus;
-    $scope.isLogged = logStatus.isLogged;
-
-    $scope.$watch('logStatus.isLogged', function (newVal, oldVal, scope) {
-      if(newVal) { 
-        scope.isLogged = newVal;
-      }
-    });
+  $scope.logStatus = logStatus;
 
   $ionicModal.fromTemplateUrl('templates/modalcreatecard.html', {
-  scope: $scope,
-  animation: 'slide-in-up'
+    scope: $scope,
+    animation: 'slide-in-up'
   }).then(function(modal) {
     $scope.modalcreatecard = modal;
   });
@@ -294,26 +257,26 @@ angular.module('starter.controllers', [])
     $scope.modalcreatecard.hide();
   };
 
-  $scope.createcard = function(card_name, first_name, last_name, phone_nbr, facebook_link, 
-        linkedin_link, email, street, city, postal_code, country, description, picture_url) {
+  $scope.createcard = function(card_name, first_name, last_name, phone_nbr, facebook_link,
+                                linkedin_link, email, street, city, postal_code, country, description, picture_url) {
 
-    return Cards.createcard(card_name, first_name, last_name, phone_nbr, facebook_link, 
-        linkedin_link, email, street, city, postal_code, country, description, picture_url, userId)
-    .then(function(response) {
+    return Cards.createcard(card_name, first_name, last_name, phone_nbr, facebook_link,
+                            linkedin_link, email, street, city, postal_code, country, description, picture_url, userId)
+      .then(function(response) {
       console.log("Card", card);
       $scope.closeModalcreatecard();
 
       $scope.$watch('currentId.userId', function (newVal, oldVal, scope) { // modification de currrentId.userId -> appelle le callback function
-      if(newVal) {
-        // console.log("hey" + newVal);
-        scope.userid = newVal;
-        userId = newVal;
-        Cards.all(newVal) //checkuser returns the userId
-        .then(function(response) {
-          scope.cards = response;
-        })
-      }
-    });
+        if(newVal) {
+          // console.log("hey" + newVal);
+          scope.userid = newVal;
+          userId = newVal;
+          Cards.all(newVal) //checkuser returns the userId
+            .then(function(response) {
+            scope.cards = response;
+          })
+        }
+      });
 
     })
   }
@@ -321,36 +284,27 @@ angular.module('starter.controllers', [])
 })
 
 .controller('LogoutCtrl', function($scope, $stateParams, $ionicModal, Users, Cards, logStatus, currentId) {
-
   $scope.logStatus = logStatus;
-  $scope.isLogged = logStatus.isLogged;
 
   $ionicModal.fromTemplateUrl('templates/modallogout.html', {
-  scope: $scope,
-  animation: 'slide-in-up'
+    scope: $scope,
+    animation: 'slide-in-up'
   }).then(function(modal) {
     $scope.modallogout = modal;
   });
 
-    // create card
+  // create card
   $scope.openModallogout = function() {
     $scope.modallogout.show();
   };
   $scope.closeModallogout = function() {
-
-    $scope.$watch('logStatus.isLogged', function (newVal, oldVal, scope) {
-      if(newVal) { 
-      scope.isLogged = newVal;
-      }
-    });
-
     $scope.$watch('currentId.userId', function (newVal, oldVal, scope) { // modification de currrentId.userId -> appelle le callback function
       if(newVal) {
         // console.log("hey" + newVal);
         scope.userid = newVal;
         userId = newVal;
         Cards.all(newVal) //checkuser returns the userId
-        .then(function(response) {
+          .then(function(response) {
           scope.cards = response;
         })
       }
@@ -361,77 +315,68 @@ angular.module('starter.controllers', [])
 
   $scope.logout = function() {
 
-      logStatus.isLogged = false;
-      currentId.userId = -1;
+    logStatus.isLogged = false;
+    currentId.userId = -1;
 
 
 
-      $scope.$watch('currentId.userId', function (newVal, oldVal, scope) { // modification de currrentId.userId -> appelle le callback function
+    $scope.$watch('currentId.userId', function (newVal, oldVal, scope) { // modification de currrentId.userId -> appelle le callback function
       if(newVal) {
         // console.log("hey" + newVal);
         scope.userid = newVal;
         userId = newVal;
         Cards.all(newVal) //checkuser returns the userId
-        .then(function(response) {
+          .then(function(response) {
           scope.cards = response;
         })
       }
     });
 
-      $scope.$watch('logStatus.isLogged', function (newVal, oldVal, scope) {
-      if(newVal) { 
-      scope.isLogged = newVal;
-    }
-
-    });
-
     $scope.closeModallogout();
-
   }
-
 })
 
 
 .controller('CardDetailCtrl', function($scope, $stateParams, Cards, logStatus) {
-  $scope.card = Cards.get($stateParams.cardId);  
+  $scope.card = Cards.get($stateParams.cardId);
 })
 
 
 .controller('GeoCtrl', function($scope, $cordovaGeolocation) {
-    
-   var posOptions = {timeout: 10000, enableHighAccuracy: false};
-    
-   $cordovaGeolocation
-   .getCurrentPosition(posOptions)
-	
-   .then(function (position) {
+
+  var posOptions = {timeout: 10000, enableHighAccuracy: false};
+
+  $cordovaGeolocation
+    .getCurrentPosition(posOptions)
+
+    .then(function (position) {
+    var lat  = position.coords.latitude
+    var long = position.coords.longitude
+    console.log(lat + '   ' + long)
+    $scope.lat = lat
+    $scope.lng=long
+  }, function(err) {
+    console.log(err)
+  });
+
+  var watchOptions = {timeout : 3000, enableHighAccuracy: false};
+  var watch = $cordovaGeolocation.watchPosition(watchOptions);
+
+  watch.then(
+    null,
+
+    function(err) {
+      console.log(err)
+    },
+
+    function(position) {
       var lat  = position.coords.latitude
       var long = position.coords.longitude
-      console.log(lat + '   ' + long)
-      $scope.lat = lat
-      $scope.lng=long
-   }, function(err) {
-      console.log(err)
-   });
+      console.log(lat + '' + long)
 
-   var watchOptions = {timeout : 3000, enableHighAccuracy: false};
-   var watch = $cordovaGeolocation.watchPosition(watchOptions);
-	
-   watch.then(
-      null,
-		
-      function(err) {
-         console.log(err)
-      },
-		
-      function(position) {
-         var lat  = position.coords.latitude
-         var long = position.coords.longitude
-         console.log(lat + '' + long)
-         
-      }
-   );
+    }
+  );
 
-   watch.clearWatch();
+  watch.clearWatch();
 
 })
